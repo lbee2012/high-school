2645d39dc5a5d9f1906a9320192eaa8d235f24b3
Long Hoang <hoangflong2012@gmail.com>
2025-07-28 14:15:14 +0700

    '7/28'

---
+++ S-6/SOF308 - Bootstrap 5 + VueJS/Code/Lab5/src/components/bai1.vue
@@ -0,0 +1,9 @@
+<script setup>
+var title = "Chao mung ban den voi VueJS"
+</script>
+
+<template>
+    <div>
+        <h2>{{ title }}</h2>
+    </div>
+</template>
---
+++ S-6/SOF308 - Bootstrap 5 + VueJS/Code/Lab5/src/components/bai6.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref } from 'vue';
+var mainClass = ref('bg-primary')
+var textClass = ref('text-white')
+</script>
+
+<style scoped>
+button {
+    background-color: cadetblue;
+    color: white;
+    border-radius: 10px;
+    padding: 5px 10px;
+    text-align: center;
+    border: none;
+}
+</style>
+
+<template>
+    <div :class="[mainClass, textClass]">
+        <h2>Mau nen vao Mau chu</h2>
+        <button @click="mainClass = 'bg-success'; textClass = 'text-dark'">Change Style</button>
+    </div>
+</template>
