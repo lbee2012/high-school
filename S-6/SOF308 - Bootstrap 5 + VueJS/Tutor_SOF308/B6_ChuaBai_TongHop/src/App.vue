40ef71c7374eef39f21ddeff12f2a88fcd740a14
Long Hoang <hoangflong2012@gmail.com>
2025-08-13 09:41:47 +0700

    update buoi6 tutor

---
+++ S-6/SOF308 - Bootstrap 5 + VueJS/Tutor_SOF308/B6_ChuaBai_TongHop/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import FooterLayout from './layout/FooterLayout.vue';
+import HeaderLayout from './layout/HeaderLayout.vue';
+import TacGiaPage from './pages/TacGiaPage.vue';
+
+</script>
+
+<template>
+  <HeaderLayout />
+  <TacGiaPage />
+  <FooterLayout />
+</template>
+
+<style scoped></style>
---
+++ S-6/SOF308 - Bootstrap 5 + VueJS/Tutor_SOF308/B6_ChuaBai_TongHop/src/components/FormTacGiaComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <section>
+        <form>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Ten</label>
+                <input type="email" class="form-control" v-model="tacGia.ten">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Tuoi</label>
+                <input type="email" class="form-control" v-model="tacGia.tuoi">
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" :value="true" v-model="tacGia.gioiTinh">
+                <label class="form-check-label" for="inlineRadio1">Nam</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" :value="false" v-model="tacGia.gioiTinh">
+                <label class="form-check-label" for="inlineRadio2">Nu</label>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Dia chi</label>
+                <input type="email" class="form-control" v-model="tacGia.diaChi">
+            </div>
+        </form>
+    </section>
+</template>
+
+<script setup>
+defineModel('tacGia', {default: {
+    ten: '',
+    tuoi: 0,
+    gioiTinh: true,
+    diaChi: '',
+}})
+</script>
+
+<style scoped>
+
+</style>
